c66d56c544008f6af4ed5df17b6b3f2a9d4ad257
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-04-24 14:31:19 -0300

    Add analyzers

--- build/index.js
+++ build/index.js
@@ -1,17 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var MatchResult_1 = require("./MatchResult");
 var MatchReader_1 = require("./MatchReader");
-var reader = new MatchReader_1.MatchReader("football.csv");
-reader.read();
-var manUnitedWins = 0;
-for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
+var csvFileReader_1 = require("./csvFileReader");
+// Create an object that satisfies the 'DataReader' interface
+var csvFileReader = new csvFileReader_1.CsvFileReader("football.csv");
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+// matchReader.matches
 console.log("Man United won " + manUnitedWins + " games");
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
 import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from "./csvFileReader";
 
-const reader = new MatchReader("football.csv");
-reader.read();
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader("football.csv");
 
-let manUnitedWins = 0;
-
-for (let match of reader.data) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+// matchReader.matches
 
 console.log(`Man United won ${manUnitedWins} games`);
